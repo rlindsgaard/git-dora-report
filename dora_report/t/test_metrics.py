4be86760107fda01b3b42cf0fb4c1a2d75f19a39
Ronni Elken Lindsgaard <ronni.lindsgaard@gmail.com>
2025-07-05 21:22:36 +0200

    fix: more typeerrors... again
    this part of the previous change got lost somehow

--- dora_report/t/test_metrics.py
+++ dora_report/t/test_metrics.py
@@ -160,9 +160,21 @@ def test_change_failure_rate(change_events, expected):
         # Case with None values
         (
             [
-                ChangeEvent("1", datetime(2023, 1, 1, 12, 0, 0), None),
-                ChangeEvent("2", datetime(2023, 1, 1, 12, 10, 0), False),
-                ChangeEvent("3", datetime(2023, 1, 1, 12, 20, 0), True),
+                ChangeEvent(
+                    identifier="1", 
+                    stamp=datetime(2023, 1, 1, 12, 0, 0), 
+                    success=None
+                ),
+                ChangeEvent(
+                    identifier="2", 
+                    stamp=datetime(2023, 1, 1, 12, 10, 0), 
+                    success=False
+                ),
+                ChangeEvent(
+                    identifier="3", 
+                    stamp=datetime(2023, 1, 1, 12, 20, 0), 
+                    success=True
+                ),
             ],
             timedelta(minutes=10),
         ),
