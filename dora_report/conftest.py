ea99fa60d43dac5531838ac74b645ab40356ba0d
Ronni Elken Lindsgaard <ronni.lindsgaard@gmail.com>
2025-07-11 21:08:39 +0200

    fix: defer caplog configuration

--- dora_report/conftest.py
+++ dora_report/conftest.py
@@ -5,5 +5,4 @@ import logging
 def root_logger(caplog):
     logging.basicConfig(level=logging.DEBUG, format="[%(levelname)s] %(message)s")
     logger = logging.getLogger(logname).setLevel(logging.DEBUG)
-    caplog.set_level(logging.DEBUG)
     return logger
