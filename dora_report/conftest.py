136f1034778d1a3717441083d4f49147cf3bc308
Ronni Elken Lindsgaard <ronni.lindsgaard@gmail.com>
2025-07-11 21:38:47 +0200

    fix: return logger object

--- dora_report/conftest.py
+++ dora_report/conftest.py
@@ -4,5 +4,6 @@ import logging
 @pytest.fixture(scope="session")
 def root_logger():
     logging.basicConfig(level=logging.DEBUG, format="[%(levelname)s] %(message)s")
-    logger = logging.getLogger("dora_report.test").setLevel(logging.DEBUG)
+    logger = logging.getLogger("dora_report.test")
+    logger.setLevel(logging.DEBUG)
     return logger
