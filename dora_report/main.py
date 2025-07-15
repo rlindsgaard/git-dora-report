05f6e0ef46e3d29e5901d4f15a45ef5e6cce37c6
Ronni Elken Lindsgaard <ronni.lindsgaard@gmail.com>
2025-07-15 22:46:43 +0200

    fix: another bad keyword invocation

--- dora_report/main.py
+++ dora_report/main.py
@@ -15,7 +15,7 @@ class DoraReport:
         
     def analyze(self):
         event_gen = self.collector.collect_change_events()
-        for chunk in chunk_interval(event_gen, start=self.since, size=self.interval_seconds, end=self.until):
+        for chunk in chunk_interval(event_gen, since=self.since, size=self.interval_seconds, until=self.until):
             # Aggregate
             record = Record(
                 start=chunk["start"],
